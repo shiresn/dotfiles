27e3c93a6c03c535db0e90f74de687bdd44582ea
thorsten chien <yungchang.chien@hpe.com>
2025-10-20 14:59:24 +0800

    remove helix alias

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,3 @@
-status --is-interactive; and source (jump shell fish | psub);export PATH="/home/thorsten/.local/bin:$PATH";alias hx=helix
+status --is-interactive; and source (jump shell fish | psub);export PATH="/home/thorsten/.local/bin:$PATH"
 
 starship init fish | source
